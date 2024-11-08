f3e24e2745da46dc7652c8b54740196e8ae7ee72
Asher Norland <asher.norland@gmail.com>
2024-11-08 12:25:12 -0500

    Update DiscussionList.vue

--- app/components/DiscussionList.vue
+++ app/components/DiscussionList.vue
@@ -1,6 +1,6 @@
 <template>
 	<Giscus
-	v-if="colorMode.preference === 'dark'"
+	id="comments"
 	repo="Behemyth/Website"
 	repo-id="R_kgDOGNxb4w"
 	:category="props.category"
@@ -10,31 +10,16 @@
 	reactions-enabled="1"
 	emit-metadata="1"
 	input-position="top"
-	theme="dark"
-	lang="en"
-	loading="lazy"
-	/>
-	<Giscus
-	v-else
-	repo="Behemyth/Website"
-	repo-id="R_kgDOGNxb4w"
-	:category="props.category"
-	category-id="DIC_kwDOGNxb484Chz_u"
-	mapping="title"
-	strict="1"
-	reactions-enabled="1"
-	emit-metadata="1"
-	input-position="top"
-	theme="light"
-	lang="en"
-	loading="lazy"
-	/>
+	:theme="theme"
+	lang="en"/>
 </template>
 
 <script setup lang="ts">
 import Giscus from '@giscus/vue'
 
 const colorMode = useColorMode()
+
+const theme = computed(() => colorMode.preference === 'dark' ? 'dark' : 'light')
 
 const props = defineProps({
 	category: {
