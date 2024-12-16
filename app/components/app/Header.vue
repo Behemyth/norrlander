9e3d799f7fab67cfb5d582af1dc954edf6d5d4a9
Asher Norland <asher.norland@gmail.com>
2024-12-16 14:14:52 -0500

    Update Header.vue

--- app/components/app/Header.vue
+++ app/components/app/Header.vue
@@ -1,37 +1,32 @@
 <template>
 	<header class="max-w-5xl w-full mx-auto p-1 bg-white shadow dark:bg-gray-900">
 		<div class="flex flex-row items-center justify-evenly md:justify-between">
-			<nav class="h-full aspect-square md:hidden">
+			<USlideover
+				side="left"
+			>
 				<UButton
 					icon="i-mdi-hamburger-menu"
-					color="primary"
 					square
 					label="Open"
-					@click="isOpen = true"
-				>
-					<span class="sr-only">Open main menu</span>
-				</UButton>
-				<USlideover
-					v-model="isOpen"
-					side="left"
-				>
+				/>
+				<template #content>
 					<UNavigationMenu
 						orientation="vertical"
 						:items="items"
-						@click="isOpen = false"
 					/>
-				</USlideover>
-			</nav>
+				</template>
+			</USlideover>
 			<ULink
 				to="/"
 				rel="author"
 				class="p-4 text-4xl font-mono font-extrabold"
 			> The Norlander
 			</ULink>
-			<nav class="hidden md:flex">
-				<UNavigationMenu :items="items" />
-			</nav>
-
+			<UNavigationMenu
+				orientation="horizontal"
+				:items="items"
+				class="hidden md:flex"
+			/>
 			<div class="md:px-8">
 				<ColorModeSwitch class="hover:text-gray-700 dark:hover:text-gray-300" />
 			</div>
@@ -40,9 +35,7 @@
 </template>
 
 <script setup lang="ts">
-const isOpen = ref(false);
-
-const items = ref([
+const items = computed(() => [
 	{
 		label: 'Reviews',
 		to: '/review',
