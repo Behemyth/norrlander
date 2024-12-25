c193f4286a79358002eb4c3121298aa9b601aef9
Asher Norland <asher.norland@gmail.com>
2024-12-24 18:18:04 -0800

    More Header Info (#439)

--- app/components/review/Header.vue
+++ app/components/review/Header.vue
@@ -2,10 +2,20 @@
 	<h1 class="prose text-3xl font-bold">
 		{{ title }}
 	</h1>
-	<ReviewStarRating
-		:value="rating"
-		:size="24"
-	/>
+	<div class="flex flex-row">
+		<ReviewStarRating
+			:value="rating"
+			:size="24"
+		/>
+		<p class="text-lg ml-2">
+			{{ releaseDate.getFullYear() }}
+		</p>
+	</div>
+	<div class="flex flex-row">
+		<p class="text-lg">
+			{{ published.toDateString() }}
+		</p>
+	</div>
 	<NuxtPicture
 		:src="`tmdb/${backdrop}`"
 		:alt="title"
@@ -25,6 +35,14 @@ defineProps({
 	},
 	rating: {
 		type: Number,
+		required: true,
+	},
+	releaseDate: {
+		type: Date,
+		required: true,
+	},
+	published: {
+		type: Date,
 		required: true,
 	},
 });
