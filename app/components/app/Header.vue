ae3a93aa727d1ce77e6c480157ec66e69fb5a2a5
Asher Norland <asher.norland@gmail.com>
2025-01-14 05:49:09 -0500

    Header Icon Grouping

--- app/components/app/Header.vue
+++ app/components/app/Header.vue
@@ -28,18 +28,20 @@
 				:items="items"
 				class="hidden md:flex justify-center"
 			/>
-			<UPopover>
-				<UButton
-					icon="i-mdi-search"
-					color="neutral"
-					variant="ghost"
-				/>
+			<div class="flex space-x-6">
+				<UPopover>
+					<UButton
+						icon="i-mdi-search"
+						color="neutral"
+						variant="ghost"
+					/>
 
-				<template #content>
-					<CollectionSearch />
-				</template>
-			</UPopover>
-			<AppColorSwitch class="md:px-8 hover:text-gray-700 dark:hover:text-gray-300" />
+					<template #content>
+						<CollectionSearch />
+					</template>
+				</UPopover>
+				<AppColorSwitch class="md:px-8 hover:text-gray-700 dark:hover:text-gray-300" />
+			</div>
 		</div>
 	</header>
 </template>
