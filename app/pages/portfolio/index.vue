86548a30899819031d8eeb617df9ce79886311bd
Asher Norland <asher.norland@gmail.com>
2025-01-14 05:51:47 -0500

    Portfolio Breadcrumbs

--- app/pages/portfolio/index.vue
+++ app/pages/portfolio/index.vue
@@ -1,9 +1,18 @@
 <template>
-	<div />
+	<div>
+		<UBreadcrumb :items="items" />
+	</div>
 </template>
 
 <script setup lang="ts">
 useSeoMeta({
 	title: 'Portfolio',
 });
+
+const items = ref([
+	{
+		label: 'Portfolio',
+		to: '/portfolio',
+	},
+]);
 </script>
