d504103de918458134d369de40ae09e4b5fd6718
Asher Norland <asher.norland@gmail.com>
2025-01-30 03:50:31 -0500

    Init Aside

--- app/layouts/content.vue
+++ app/layouts/content.vue
@@ -1,10 +1,19 @@
 <template>
-	<DPage>
-		<div class="flex grow">
-			<slot />
-		</div>
-	</DPage>
+	<UContainer>
+		<DPage>
+			<div class="flex flex-col grow">
+				<slot />
+			</div>
+
+			<template #left>
+				<DPageAside>
+					<DContentSectionList :toc="$attrs.toc as Toc" />
+				</DPageAside>
+			</template>
+		</DPage>
+	</UContainer>
 </template>
 
 <script setup lang="ts">
+import type { Toc } from '@nuxt/content';
 </script>
--- app/pages/review/index.vue
+++ app/pages/review/index.vue
@@ -1,30 +1,37 @@
 <template>
 	<div>
-		<UBreadcrumb
-			:items="items"
-			class="m-2"
-		/>
+		<NuxtLayout
+			name="content"
+			:toc="toc"
+		>
+			<UBreadcrumb
+				:items="items"
+				class="m-2"
+			/>
 
-		<ContentRenderer
-			:value="page"
-			:prose="true"
-		/>
+			<ContentRenderer
+				:value="page"
+				:prose="true"
+			/>
 
-		<ProseH2>
-			Recent Movie Reviews
-		</ProseH2>
+			<ProseH2>
+				Recent Movie Reviews
+			</ProseH2>
 
-		<ReviewMovieCardGrid :movies="movies" />
+			<ReviewMovieCardGrid :movies="movies" />
 
-		<ProseH2>
-			Recent Show Reviews
-		</ProseH2>
+			<ProseH2>
+				Recent Show Reviews
+			</ProseH2>
 
-		<ReviewShowCardGrid :shows="shows" />
+			<ReviewShowCardGrid :shows="shows" />
+		</NuxtLayout>
 	</div>
 </template>
 
 <script setup lang="ts">
+import type { Toc } from '@nuxt/content';
+
 const movies = await queryCollection('movie')
 	.order('date_published', 'DESC').limit(6).all();
 
@@ -33,11 +40,15 @@ const shows = await queryCollection('show')
 
 const page = await queryCollection('page').where('title', '=', 'Review').first();
 
+definePageMeta({
+	layout: false,
+});
+
 useSeoMeta({
 	title: 'Reviews',
 });
 
-const items = ref([
+const items = [
 	{
 		label: 'Home',
 		to: '/',
@@ -46,5 +57,12 @@ const items = ref([
 		label: 'Reviews',
 		to: '/review',
 	},
-]);
+];
+
+const toc: Toc = {
+	title: 'Table of Contents',
+	links: [],
+	depth: 0,
+	searchDepth: 0,
+};
 </script>
