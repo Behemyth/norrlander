4fd2e31d82dd9e3b8ccabdca59c2ed0d8461afbd
Asher Norland <asher.norland@gmail.com>
2025-02-17 00:51:23 -0500

    Create Card.vue

---
+++ modules/deermice-ui/runtime/components/Card.vue
@@ -0,0 +1,30 @@
+<template>
+	<div :class="theme.base()">
+		<slot />
+	</div>
+</template>
+
+<script lang="ts">
+import { tv } from 'tailwind-variants';
+
+const baseTheme = tv({
+	slots: {
+		base: 'mt-8 pb-24 space-y-12',
+	},
+});
+
+export interface CardProps {
+	description?: string;
+}
+
+export interface CardSlots {
+	default(props?: object): any;
+}
+</script>
+
+<script setup lang="ts">
+defineProps<CardProps>();
+defineSlots<CardSlots>();
+
+const theme = baseTheme();
+</script>
