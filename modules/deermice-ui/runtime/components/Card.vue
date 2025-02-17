bb7f7176867c9b02c0ed2b2106ba81008be93b0c
Asher Norland <asher.norland@gmail.com>
2025-02-17 00:59:53 -0500

    Card Styling

--- modules/deermice-ui/runtime/components/Card.vue
+++ modules/deermice-ui/runtime/components/Card.vue
@@ -1,29 +1,71 @@
 <template>
-	<div :class="theme.base()">
+	<div :class="theme.root()">
 		<slot />
 	</div>
 </template>
 
 <script lang="ts">
+import type { PartialString } from '@nuxt/ui/runtime/types/utils.js';
 import { tv } from 'tailwind-variants';
 
 const baseTheme = tv({
 	slots: {
-		base: 'mt-8 pb-24 space-y-12',
+		root: 'relative group flex rounded-[calc(var(--ui-radius)*2)]',
+		container: 'relative flex flex-col flex-1 lg:grid gap-x-8 gap-y-4 p-4 sm:p-6',
+		wrapper: '',
+		header: 'mb-4',
+		body: '',
+		footer: 'mt-4',
+		title: 'text-base text-pretty font-semibold text-(--ui-text-highlighted)',
+		description: 'text-[15px] text-pretty',
+	},
+	variants: {
+		orientation: {
+			horizontal: {
+				container: 'lg:grid-cols-2 lg:items-center',
+			},
+			vertical: {
+				container: '',
+			},
+		},
+		reverse: {
+			true: {
+				wrapper: 'lg:order-last',
+			},
+		},
+		title: {
+			true: {
+				description: 'mt-1',
+			},
+		},
 	},
 });
 
 export interface CardProps {
+	title?: string;
 	description?: string;
+	orientation: 'vertical' | 'horizontal';
+	reverse?: boolean;
+	ui?: PartialString<typeof baseTheme.slots>;
 }
 
 export interface CardSlots {
+	header(props?: object): any;
+	body(props?: object): any;
+	title(props?: object): any;
+	description(props?: object): any;
+	footer(props?: object): any;
 	default(props?: object): any;
 }
 </script>
 
 <script setup lang="ts">
-defineProps<CardProps>();
+withDefaults(defineProps<CardProps>(),
+	{
+		orientation: 'vertical',
+		reverse: false,
+	},
+);
 defineSlots<CardSlots>();
 
 const theme = baseTheme();
