711c8d264e38a67d6c101da45a1a1116f6230147
Asher Norland <asher.norland@gmail.com>
2025-02-16 19:26:09 -0500

    Collection Links

--- app/components/content/CollectionGrid.vue
+++ app/components/content/CollectionGrid.vue
@@ -1,25 +1,28 @@
 <template>
 	<div class="grid grid-flow-row gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
-		<LazyUCard
+		<ULink
 			v-for="element in elements"
 			:key="element.id"
+			:to="element.path"
 		>
-			<template #header>
-				<ProseH3>
-					{{ element[titleValue] }}
-				</ProseH3>
-			</template>
-		</LazyUCard>
+			<LazyUCard>
+				<template #header>
+					<ProseH3>
+						{{ element[titleValue] }}
+					</ProseH3>
+				</template>
+			</LazyUCard>
+		</ULink>
 	</div>
 </template>
 
-<script setup lang="ts" generic="T extends keyof Collections">
-import type { Collections } from '@nuxt/content';
+<script setup lang="ts" generic="T extends keyof PageCollections">
+import type { PageCollections } from '@nuxt/content';
 
 const props = defineProps<{
 	collection: T;
-	titleValue: keyof Collections[T];
-	orderValue?: keyof Collections[T];
+	titleValue: keyof PageCollections[T];
+	orderValue?: keyof PageCollections[T];
 	count?: number;
 }>();
 
