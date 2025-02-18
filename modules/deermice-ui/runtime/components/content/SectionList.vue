bac6ab1650f3368dd0eb67c6a767444180c3d7d5
Asher Norland <asher.norland@gmail.com>
2025-02-18 06:37:44 -0500

    SectionList Styling

--- modules/deermice-ui/runtime/components/content/SectionList.vue
+++ modules/deermice-ui/runtime/components/content/SectionList.vue
@@ -3,38 +3,38 @@
 		v-if="toc.title || toc.links.length"
 		:class="theme.root({ class: ui?.root })"
 	>
-		<ProseH4
+		<p
 			v-if="toc.title"
 			:class="theme.title({ class: ui?.title })"
 		>
 			{{ toc.title }}
-		</ProseH4>
+		</p>
 		<nav
 			v-if="toc.links.length"
 			:class="theme.container({ class: ui?.container })"
 		>
-			<ProseUl :class="theme.list({ class: ui?.list })">
-				<ProseLi
+			<div :class="theme.list({ class: ui?.list })">
+				<div
 					v-for="section in toc.links"
 					:key="section.id"
 					@click="onClick(section.id)"
 				>
-					<ProseP :class="theme.link({ class: ui?.link })">
+					<p :class="theme.link({ class: ui?.link })">
 						{{ section.text }}
-					</ProseP>
-					<ProseUl>
-						<ProseLi
+					</p>
+					<div :class="theme.subList({ class: ui?.subList })">
+						<div
 							v-for="link in section.children"
 							:key="link.id"
 							@click.stop="onClick(link.id)"
 						>
-							<ProseP :class="theme.link({ class: ui?.link })">
+							<p :class="theme.link({ class: ui?.link })">
 								{{ link.text }}
-							</ProseP>
-						</ProseLi>
-					</ProseUl>
-				</ProseLi>
-			</ProseUl>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
 		</nav>
 		<slot />
 	</div>
@@ -49,18 +49,13 @@ const baseTheme = tv({
 	slots: {
 		root: [
 			'sticky top-[calc(var(--ui-header-height)+1px)] bg-[var(--ui-bg)]/75',
-			'backdrop-blur px-4 sm:px-6 overflow-y-auto',
+			'backdrop-blur px-2 overflow-y-auto',
 			'max-h-[calc(100vh-var(--ui-header-height))]',
 		],
-		container: [
-			'pt-4 sm:pt-6 pb-2.5 sm:pb-4.5 lg:py-8 border-b',
-			'border-dashed border-[var(--ui-border)] lg:border-0 flex flex-col',
-		],
+		container: 'py-2 flex flex-col',
 		title: 'truncate',
 		list: '',
-		listWithChildren: 'ms-3',
-		item: '',
-		itemWithChildren: '',
+		subList: 'ms-3',
 		link: 'group text-sm block truncate focus-visible:outline-[var(--ui-primary)] py-1',
 	},
 	variants: {
