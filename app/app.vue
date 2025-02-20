1b17ca209dace37ff7372e05a836b029b2242369
Asher Norland <asher.norland@gmail.com>
2025-02-20 03:29:49 -0500

    Move Feeds Back to App (#480)

--- app/app.vue
+++ app/app.vue
@@ -10,6 +10,28 @@
 	</DApp>
 </template>
 
+<script setup lang="ts">
+const links
+	= await queryCollection('content')
+		.where('feed', 'IS NOT NULL')
+		.all()
+		.then((value) => {
+			return value.map((page) => {
+				return {
+					rel: 'alternate',
+					title: page.title,
+					type: 'application/json',
+					href: 'https://ashernorland.com/feed' + page.path + '.json',
+				};
+			});
+		});
+
+// These links are for external services, and will not be picked up by the prerender (not relative)
+useHead({
+	link: links,
+});
+</script>
+
 <style>
 @import "tailwindcss";
 @import "@nuxt/ui";
