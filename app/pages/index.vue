c5747944b00841a73cea23f408a6dd818ae41c01
Asher Norland <asher.norland@gmail.com>
2025-02-20 03:29:25 -0500

    Move Feeds Back to App

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,26 +3,6 @@
 </template>
 
 <script lang="ts" setup>
-const links
-	= await queryCollection('content')
-		.where('feed', 'IS NOT NULL')
-		.all()
-		.then((value) => {
-			return value.map((page) => {
-				return {
-					rel: 'alternate',
-					title: page.title,
-					type: 'application/json',
-					href: 'https://ashernorland.com/feed' + page.path + '.json',
-				};
-			});
-		});
-
-// These links are for external services, and will not be picked up by the prerender (not relative)
-useHead({
-	link: links,
-});
-
 useSeoMeta({
 	title: 'The Norlander',
 });
