d728f26c794fc99b0e90e99fc07a83558f12361f
Asher Norland <asher.norland@gmail.com>
2025-06-20 20:20:34 -0400

    yaaaa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,12 @@
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
-	extends: [
-		'@deermice/ui',
-	],
-
 	// nuxtjs/content should be loaded after 'nuxt/ui' or 'deermice-ui'
 	// nuxtjs/sitemap and robots should be loaded before 'nuxt/content'
 	modules: [
 		'@nuxtjs/sitemap',
 		'@nuxtjs/robots',
 		'@nuxt/ui',
+		'@deermice/ui',
 		'@nuxt/content',
 		'@nuxt/image',
 		'@nuxtjs/i18n',
