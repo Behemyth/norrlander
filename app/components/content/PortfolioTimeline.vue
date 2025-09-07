812686fa2c8a6a7fc50a617e2df80a46fec49d78
Asher Norland <asher.norland@gmail.com>
2025-09-07 14:17:21 -0400

    Update Timeline w/ Academics

--- app/components/content/PortfolioTimeline.vue
+++ app/components/content/PortfolioTimeline.vue
@@ -1,15 +1,54 @@
 <template>
 	<UPageList>
-		<CareerCard
+		<template
 			v-for="item in items"
 			:key="item.path"
-			:job="item"
-		/>
+		>
+			<CareerCard
+				v-if="isCareerItem(item)"
+				:job="item"
+			/>
+			<AcademicCard
+				v-else-if="isAcademicItem(item)"
+				:academic="item"
+			/>
+		</template>
 	</UPageList>
 </template>
 
 <script setup lang="ts">
-const items = await queryCollection('career')
-	.order('start_date', 'DESC')
-	.all();
+import type { CareerCollectionItem, AcademicCollectionItem } from '@nuxt/content';
+
+async function getCareersFromCollection() {
+	return queryCollection('career')
+		.order('start_date', 'DESC')
+		.all();
+}
+
+async function getAcademicsFromCollection() {
+	return queryCollection('academic')
+		.order('start_date', 'DESC')
+		.all();
+}
+
+async function getAllPortfolioItems() {
+	const [careers, academics] = await Promise.all([
+		getCareersFromCollection(),
+		getAcademicsFromCollection(),
+	]);
+
+	return [...careers, ...academics]
+		.sort((a, b) => new Date(b.start_date).getTime() - new Date(a.start_date).getTime());
+}
+
+const items = await getAllPortfolioItems();
+
+// Type guard to determine item type
+function isCareerItem(item: CareerCollectionItem | AcademicCollectionItem): item is CareerCollectionItem {
+	return 'position' in item;
+}
+
+function isAcademicItem(item: CareerCollectionItem | AcademicCollectionItem): item is AcademicCollectionItem {
+	return !('position' in item);
+}
 </script>
