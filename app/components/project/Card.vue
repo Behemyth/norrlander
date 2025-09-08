87f2b919fac28a2b8628a1bcc33eb67201fabfba
Asher Norland <asher.norland@gmail.com>
2025-09-08 01:36:02 -0400

    Stub Project Grid

---
+++ app/components/project/Card.vue
@@ -0,0 +1,26 @@
+<template>
+	<UPageCard
+		:title="project.title"
+		:description="project.description"
+		:to="project.path"
+		variant="ghost"
+	>
+		<UPageList>
+			<UPageFeature
+				v-if="project.link"
+				icon="i-heroicons-link"
+				:description="project.link"
+			/>
+		</UPageList>
+	</UPageCard>
+</template>
+
+<script setup lang="ts">
+import type { ProjectCollectionItem } from '@nuxt/content';
+
+interface Props {
+	project: ProjectCollectionItem;
+}
+
+const { project } = defineProps<Props>();
+</script>
