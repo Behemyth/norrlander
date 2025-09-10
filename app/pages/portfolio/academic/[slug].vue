93c172947c4613f6d82399ef901cff3f26fbf3ed
Asher Norland <asher.norland@gmail.com>
2025-09-10 17:00:31 -0400

    Update Pages Aside

--- app/pages/portfolio/academic/[slug].vue
+++ app/pages/portfolio/academic/[slug].vue
@@ -1,8 +1,11 @@
 <template>
 	<UPage
 		v-if="page"
-		class="max-w-4xl mx-auto w-full"
 	>
+		<UPageHeader
+			:title="page.title"
+			:description="page.description"
+		/>
 		<UPageBody>
 			<article>
 				<ContentRenderer
@@ -10,6 +13,20 @@
 				/>
 			</article>
 		</UPageBody>
+		<template
+			v-if="page.body.toc && page.body.toc.links.length"
+			#left
+		>
+			<UPageAside>
+				<UContentToc
+					title="Contents"
+					highlight
+					highlight-color="neutral"
+					color="neutral"
+					:links="page.body.toc.links"
+				/>
+			</UPageAside>
+		</template>
 	</UPage>
 </template>
 
