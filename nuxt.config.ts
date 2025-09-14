ea26b12c584a266fcb38b40369fd55e7fd874d52
Asher Norland <asher.norland@gmail.com>
2025-09-14 11:44:44 -0400

    Remove Custom Color Definitions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -48,12 +48,6 @@ export default defineNuxtConfig({
 			sqliteConnector: 'native',
 		},
 	},
-	// https://ui.nuxt.com/getting-started/theming
-	ui: {
-		theme: {
-			colors: ['primary', 'secondary', 'neutral'],
-		},
-	},
 
 	runtimeConfig: {
 		apiSecret: '', // can be overridden by NUXT_API_SECRET environment variable
