aa83fe0fa477926649c32bc1af0c7950c9ecd715
Asher Norland <asher.norland@gmail.com>
2025-09-16 17:56:42 -0400

    Update Header.vue

--- app/components/review/Header.vue
+++ app/components/review/Header.vue
@@ -1,43 +1,52 @@
 <template>
 	<div class="flex flex-col">
-		<div class="flex flex-row">
-			<h1 class="text-3xl font-bold">
-				{{ content.title }}
-			</h1>
-		</div>
-		<div class="flex flex-row">
-			<ReviewStarRating
-				:value="content.rating"
-				:size="24"
-				class="mr-2"
-			/>
-			<p class="text-lg mx-2">
-				Release Year
-			</p>
-			<p class="text-lg mx-2">
-				Genre
-			</p>
-			<p class="text-lg mx-2">
-				Runtime
-			</p>
-		</div>
-		<div class="flex flex-row">
-			<p class="text-lg mr-2">
-				Asher Norland
-			</p>
-			<p class="text-lg mx-2">
-				Published Date
-			</p>
-			<p class="text-lg mx-2">
-				Time to Read
-			</p>
-		</div>
+		<UPageHeader
+			:title="content.title"
+			:ui="{ root: 'pb-4' }"
+		>
+			<template #title>
+				<div class="flex flex-col">
+					<ProseH1 class="!mb-2 text-3xl font-bold">
+						{{ content.title }}
+					</ProseH1>
+					<div class="flex flex-row items-center mt-1 flex-wrap">
+						<ReviewStarRating
+							:value="content.rating"
+							:size="24"
+							class="mr-4"
+						/>
+						<ProseP class="!my-0 text-lg mx-2">
+							Release Year
+						</ProseP>
+						<ProseP class="!my-0 text-lg mx-2">
+							Genre
+						</ProseP>
+						<ProseP class="!my-0 text-lg mx-2">
+							Runtime
+						</ProseP>
+					</div>
+				</div>
+			</template>
+
+			<template #description>
+				<div class="flex flex-row flex-wrap items-center gap-x-4 mt-2">
+					<ProseP class="!my-0 text-lg">
+						Asher Norland
+					</ProseP>
+					<ProseP class="!my-0 text-lg">
+						Published Date
+					</ProseP>
+					<ProseP class="!my-0 text-lg">
+						Time to Read
+					</ProseP>
+				</div>
+			</template>
+		</UPageHeader>
+
 		<NuxtPicture
 			:src="`tmdb/${content.tmdbData.backdrop_path}`"
 			:alt="content.title"
-			:img-attrs="{
-				class: 'object-cover w-full aspect-[12/5]',
-			}"
+			:img-attrs="{ class: 'object-cover w-full aspect-[12/5]' }"
 		/>
 	</div>
 </template>
