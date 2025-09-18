b289b274db0198e6ec4a445c0cf58779ae7b95e6
Asher Norland <asher.norland@gmail.com>
2025-09-18 10:26:02 -0400

    Default Layout

--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -1,60 +1,64 @@
 <template>
-	<UPage>
-		<template #left>
-			<UPageAside class="hidden lg:block">
-				<AppProfileCard
-					orientation="vertical"
-					variant="ghost"
-					footer
-				/>
-			</UPageAside>
-		</template>
-		<UPageBody>
-			<UPageColumns>
-				<AppProfileCard
-					orientation="horizontal"
-					class="lg:hidden"
-					footer
-				/>
-				<UPageCard
-					:title="$t('contact')"
-					description="Use responsibly"
-				>
-					<UPageList divide>
-						<UPageFeature
-							v-for="(info, index) in contact"
-							:key="index"
-							class="m-2"
+	<div>
+		<NuxtLayout name="default">
+			<UPage>
+				<template #left>
+					<UPageAside class="hidden lg:block">
+						<AppProfileCard
+							orientation="vertical"
 							variant="ghost"
-							:title="info.name"
-							:icon="info.icon"
-							:to="info.link"
-							target="_blank"
-							:description="info.description"
+							footer
 						/>
-					</UPageList>
-				</UPageCard>
-				<UPageCard
-					title="Socials"
-					description="Here I dabble"
-				>
-					<UPageList divide>
-						<UPageFeature
-							v-for="(social, index) in socials"
-							:key="index"
-							class="m-2"
-							variant="ghost"
-							:title="social.name"
-							:icon="social.icon"
-							:to="social.link"
-							target="_blank"
-							:description="social.description"
+					</UPageAside>
+				</template>
+				<UPageBody>
+					<UPageColumns>
+						<AppProfileCard
+							orientation="horizontal"
+							class="lg:hidden"
+							footer
 						/>
-					</UPageList>
-				</UPageCard>
-			</UPageColumns>
-		</UPageBody>
-	</UPage>
+						<UPageCard
+							:title="$t('contact')"
+							description="Use responsibly"
+						>
+							<UPageList divide>
+								<UPageFeature
+									v-for="(info, index) in contact"
+									:key="index"
+									class="m-2"
+									variant="ghost"
+									:title="info.name"
+									:icon="info.icon"
+									:to="info.link"
+									target="_blank"
+									:description="info.description"
+								/>
+							</UPageList>
+						</UPageCard>
+						<UPageCard
+							title="Socials"
+							description="Here I dabble"
+						>
+							<UPageList divide>
+								<UPageFeature
+									v-for="(social, index) in socials"
+									:key="index"
+									class="m-2"
+									variant="ghost"
+									:title="social.name"
+									:icon="social.icon"
+									:to="social.link"
+									target="_blank"
+									:description="social.description"
+								/>
+							</UPageList>
+						</UPageCard>
+					</UPageColumns>
+				</UPageBody>
+			</UPage>
+		</NuxtLayout>
+	</div>
 </template>
 
 <script lang="ts" setup>
@@ -62,6 +66,6 @@ const { data: contact } = await useContactData();
 const { data: socials } = await useSocialsData();
 
 definePageMeta({
-	layout: 'default',
+	layout: false,
 });
 </script>
