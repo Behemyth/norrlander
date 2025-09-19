6d18a83bca4c4db184a06a0f656a7949a8523e4e
Asher Norland <asher.norland@gmail.com>
2025-09-19 04:52:38 -0400

    Mold I - Content

--- app/components/photography/Card.vue
+++ app/components/photography/Card.vue
@@ -3,8 +3,19 @@
 		:title="photo.title"
 		:description="photo.description"
 		:to="photo.path"
-		variant="ghost"
-	/>
+		variant="outline"
+	>
+		<NuxtImg
+			v-if="photo.images?.[0]"
+			:src="photo.images[0].src"
+			:alt="photo.images[0].alt"
+			:width="photo.images[0].width"
+			:height="photo.images[0].height"
+			:sizes="photo.images[0].sizes"
+			:densities="photo.images[0].densities"
+			class="w-full h-auto object-cover"
+		/>
+	</UPageCard>
 </template>
 
 <script setup lang="ts">
